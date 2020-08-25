acad5afe8aa55a5c892f8e58ade81eb0112d2887
Jan Hovancik <conta.srdr@gmail.com>
2020-08-25 14:02:54 +0200

    WIP Break window size

--- app/main.js
+++ app/main.js
@@ -359,9 +359,14 @@ function startMicrobreak () {
   }
 
   for (let displayIdx = 0; displayIdx < numberOfDisplays(); displayIdx++) {
+    const electron = require('electron')
+    const screens = electron.screen.getAllDisplays()
+    const theScreen = screens[displayIdx]
+    const { width, height } = theScreen.workAreaSize
+
     const windowOptions = {
-      width: 800,
-      height: 600,
+      width: Number.parseInt(width * 0.8),
+      height: Number.parseInt(height * 0.8),
       autoHideMenuBar: true,
       icon: windowIconPath(),
       resizable: false,
@@ -406,6 +411,9 @@ function startMicrobreak () {
       microbreakWinLocal.webContents.send('progress', startTime,
         breakDuration, strictMode, postponable, postponableDurationPercent, settings.get('endBreakShortcut'))
       microbreakWinLocal.setAlwaysOnTop(true)
+      setTimeout(() => {
+        microbreakWinLocal.center()
+      }, 0)
     })
     microbreakWinLocal.loadURL(modalPath)
     microbreakWinLocal.setVisibleOnAllWorkspaces(true)
@@ -465,9 +473,14 @@ function startBreak () {
   }
 
   for (let displayIdx = 0; displayIdx < numberOfDisplays(); displayIdx++) {
+    const electron = require('electron')
+    const screens = electron.screen.getAllDisplays()
+    const theScreen = screens[displayIdx]
+    const { width, height } = theScreen.workAreaSize
+
     const windowOptions = {
-      width: 800,
-      height: 600,
+      width: Number.parseInt(width * 0.8),
+      height: Number.parseInt(height * 0.8),
       autoHideMenuBar: true,
       icon: windowIconPath(),
       resizable: false,
@@ -512,6 +525,9 @@ function startBreak () {
       breakWinLocal.webContents.send('progress', startTime,
         breakDuration, strictMode, postponable, postponableDurationPercent, settings.get('endBreakShortcut'))
       breakWinLocal.setAlwaysOnTop(true)
+      setTimeout(() => {
+        breakWinLocal.center()
+      }, 0)
     })
     breakWinLocal.loadURL(modalPath)
     breakWinLocal.setVisibleOnAllWorkspaces(true)
